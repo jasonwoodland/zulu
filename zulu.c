752e1160e4ad96ca43cd45781f5960d503a6401c
Jason Woodland <me@jasonwoodland.com>
2022-12-08 23:16:54 +1100

    feat: debug flag

--- zulu.c
+++ zulu.c
@@ -2,10 +2,17 @@
 #include <time.h>
 #include <unistd.h>
 
-#define micros() (clock() / 10) // slow debug clock rate
-/* #define micros() (clock() * 1000000 / CLOCKS_PER_SEC) */
+/* #define DEBUG 1 */
 
-void noop() {}
+#ifdef DEBUG
+#define micros() (clock() / 10) // slow debug clock rate
+#else
+#define micros() (clock() * 1000000 / CLOCKS_PER_SEC)
+#endif
+
+#define debug_print(...) do { if (DEBUG) fprintf(stderr, __VA_ARGS__); } while (0)
+
+void nop() {}
 
 int op_cycle_start_tm = 0;
 int op_inc_tm = 0;
@@ -14,19 +21,30 @@ int op_idx = 0;
 
 int rx_state = 0;
 
+int dbg_cycle_cnt = 0;
+
 void op_start() {
   if (op_idx == op_inc_idx) {
+    #ifdef DEBUG
+    debug_print("cycles %d\n", dbg_cycle_cnt);
+    dbg_cycle_cnt = 0;
+    #endif
+
     op_idx = 0;
     op_cycle_start_tm = micros();
     op_inc_tm = 0;
   }
+
   op_inc_idx = 0;
+
+  #ifdef DEBUG
+  dbg_cycle_cnt++;
+  #endif
 }
 
 void op_at(int us, void (*fn)()) {
-  /* printf("op_at      accul time: %d    inc_idx: %d    idx: %d\n", op_inc_tm + us, op_inc_idx, op_idx); */
+  /* printf("op_at	accul time: %d	inc_idx: %d	idx: %d\n", op_inc_tm + us, op_inc_idx, op_idx); */
   if (op_inc_idx == op_idx && micros() - op_cycle_start_tm >= op_inc_tm + us) {
-    /* printf("fire\n"); */
     fn();
     op_inc_tm += us;
     op_idx++;
@@ -35,7 +53,7 @@ void op_at(int us, void (*fn)()) {
 }
 
 void op_wait(int us) {
-  op_at(us, noop);
+  op_at(us, nop);
 }
 
 void rx_off() {
@@ -53,8 +71,9 @@ void toggle_rx_at(int us) {
 
 void main_cycle() {
   while (1) {
+    #ifdef DEBUG
     usleep(100);
-    /* printf("--- %lu\n", micros() - op_cycle_start_tm); */
+    #endif
     op_start();
     toggle_rx_at(0);
     toggle_rx_at(240);
